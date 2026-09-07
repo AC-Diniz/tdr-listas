a9bd2b265b1b6d10502e24841ce378c216a801b6
Ananda Diniz <anandacdiniz@gmail.com>
2026-09-07 12:04:05 -0300

    style: figura.R corrigida com styler

--- listas/lista-01/figura.R
+++ listas/lista-01/figura.R
@@ -3,12 +3,12 @@ dados$Month <- factor(dados$Month)
 
 pdf("figura.pdf", width = 7, height = 5)
 boxplot(
-Ozone ~ Month,
-data = dados,
-main = "Distribuição do Ozônio por mês",
-xlab = "Mês",
-ylab = "Ozônio (ppb)",
-col = "lightblue",
-border = "darkblue"
+  Ozone ~ Month,
+  data = dados,
+  main = "Distribuição do Ozônio por mês",
+  xlab = "Mês",
+  ylab = "Ozônio (ppb)",
+  col = "lightblue",
+  border = "darkblue"
 )
 dev.off()
