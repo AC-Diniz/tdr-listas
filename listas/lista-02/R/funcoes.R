5a82c85e58061a596f45f1a9d27abce4125ab1da
Ananda Diniz <anandacdiniz@gmail.com>
2026-09-14 18:30:57 -0300

    Exercício 2: atualização de fucoes.R

--- listas/lista-02/R/funcoes.R
+++ listas/lista-02/R/funcoes.R
@@ -1,29 +1,33 @@
-# Ler o csv e acrescentar o nome do mês, que as tabelas e os gráficos usam
-ler_dados <- function(arquivo){
+## Lê o CSV e acrescenta o nome do mês, que as tabelas e os gráficos usam.
+ler_dados <- function(arquivo) {
   dados <- read.csv(arquivo)
   dados$Mes <- factor(dados$Month, levels = 5:9,
-                      labels = c("Maio", "Junho", "Julho", "Agosto", "Setembro"))
+                      labels = c("Maio", "Junho", "Julho", "Agosto",
+                                 "Setembro"))
   dados
 }
 
-## Média mensal de cada variável, com os dias em que ela foi medida
+## Média mensal de cada variável, com os dias em que ela foi medida.
 medias_mensais <- function(dados) {
-  medias <- aggregate(cbind(Ozone, Solar.R) ~ Mes, data = dados, FUN = mean, na.action = na.pass, na.rm = TRUE)
-  medias [, -1] <- round(medias[, -1], 1)
+  medias <- aggregate(cbind(Ozone, Solar.R) ~ Mes, data = dados, FUN = mean,
+                      na.action = na.pass, na.rm = TRUE)
+  medias[, -1] <- round(medias[, -1], 1)
   medias
 }
 
-## Regressão do ozônio sobre a radiação solar
+## Regressão do ozônio sobre a temperatura, nos dias com as duas medidas.
 ajustar_modelo <- function(dados) {
   lm(Ozone ~ Solar.R, data = dados)
 }
 
-## Desenha a dispersão com a reta ajustada e devolve o caminho do arquivo
+## Desenha a dispersão com a reta ajustada e devolve o caminho do arquivo,
+## que é o que o alvo com format = "file" registra.
 salvar_figura <- function(dados, modelo, arquivo = "saidas/dispersao.png") {
   dir.create(dirname(arquivo), showWarnings = FALSE, recursive = TRUE)
   png(arquivo, width = 1400, height = 900, res = 180)
   on.exit(dev.off())
-  plot(Ozone ~ Solar.R, data = dados, pch = 20, col = "steelblue", xlab= "Radiação Solar (lang)", ylab = "Ozônio (ppb)")
-  abline(modelo, col = "tomato", lwd =2)
+  plot(Ozone ~ Temp, data = dados, pch = 20, col = "steelblue",
+       xlab = "Radiação Solar (lang)", ylab = "Ozônio (ppb)")
+  abline(modelo, col = "tomato", lwd = 2)
   arquivo
 }
